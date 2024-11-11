b3253731fb77a4d006de56a4226e4f629bad0897
preethisribt <preethisribt@gmail.com>
2024-11-11 17:59:32 +1100

    Added .env file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig, devices } from '@playwright/test';
+import {config} from "dotenv";
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
+config();
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
