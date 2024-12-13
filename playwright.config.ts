f6a331c0b40ecc082ada4b850c5d0f3aa2d59ecb
preethisribt <preethisribt@gmail.com>
2024-12-13 16:32:46 +1100

    updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   testDir: "./tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 1,
+  retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 1 : 2,
   reporter: "html",
 
@@ -17,7 +17,8 @@ export default defineConfig({
     trace: "retain-on-failure",
     launchOptions:{
       args: ["--start-maximized"],
-    }
+    },
+    video:"retain-on-failure"
   },
   
   projects: [
