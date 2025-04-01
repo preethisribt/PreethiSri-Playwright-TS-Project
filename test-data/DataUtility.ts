654f8b96fa56f89446867f6dfb1e259e3af4e871
preethisribt <preethisribt@gmail.com>
2025-04-01 19:07:52 +1100

    updated utility

--- test-data/DataUtility.ts
+++ test-data/DataUtility.ts
@@ -1,6 +1,14 @@
+import { faker } from "@faker-js/faker";
 import { parse } from "csv-parse/sync";
 import fs from "fs";
+const date: Date = new Date();
+const checkinDate: string = date.toISOString().split("T")[0];
+const checkoutDate: string = new Date(date.setDate(date.getDate() + 7))
+    .toISOString()
+    .split("T")[0];
 
+const fname: string = faker.person.firstName();
+const lname: string = faker.person.lastName();
 export interface CustomerData {
     dataID: string,
     category: string,
@@ -32,4 +40,19 @@ export class DataUtility {
             skip_empty_lines: true
         });
     }
+
+    apiTestData = (overrides = {}) => {
+        const data = JSON.parse(fs.readFileSync("test-data/API data/BookerPayload.json", "utf-8"));
+        return { ...data, ...overrides };
+    }
+
+
+    payloadData = this.apiTestData({
+        firstname: fname,
+        lastname: lname,
+        bookingdates: {
+            checkin: checkinDate,
+            checkout: checkoutDate
+        }
+    });
 }
